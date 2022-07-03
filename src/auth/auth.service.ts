178b66b51d7690f178297da14326e4ff5b091b6a
unknown <ukaran11@gmail.com>
2022-07-03 21:11:04 +0530

    Utsav | Created getTokens helper func

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,48 @@
 import { Injectable } from '@nestjs/common';
+
 import { AuthDto } from 'src/auth/dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
 import { Tokens } from './types';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
 
   hashData(data: string) {
     return bcrypt.hash(data, 10);
   }
 
+  async getTokens(userId: number, email: string) : Promise<Tokens> {
+    const [at, rt] = await Promise.all([
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email
+        },
+        {
+          secret: 'at-secret',
+          expiresIn: 60 * 15
+        }
+      ),
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email
+        },
+        {
+          secret: 'rt-secret',
+          expiresIn: 60 * 60 * 24 * 7 
+        }
+      ),
+    ]);
+
+    return {
+      access_token: at,
+      refresh_token: rt
+    }
+    
+  }
   async signupLocal(dto: AuthDto): Promise<Tokens> {
     const hash = await this.hashData(dto.password);
 
@@ -19,7 +51,11 @@ export class AuthService {
         email: dto.email,
         hash,
       }
-    })
+    });
+
+    const tokens = await this.getTokens(newUser.id, newUser.email);
+
+    return tokens;
   }
 
   signinLocal() {}
